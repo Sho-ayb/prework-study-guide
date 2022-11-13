1b79864c61cbd50ea0df19534cdf6743b942c406
Sho.ayb <Sho.ayb@outlook.com>
2022-11-13 04:01:10 +0000

    Added more Javascript code: functions: listTopics(), selectTopic()

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -18,7 +18,7 @@ console.log(topics);
 
 // lets create a variable here
 
-var topic = "HTML";
+// var topic = "HTML";
 
 // lets create a conditional statement that prints an output to the console, if condition is truthy.
 
@@ -36,6 +36,9 @@ if (topic === "HTML") {
   console.log("Please try again !");
 }
 */
+
+/*
+
 // creating an array variable
 
 var shapes = ["circle", "triangle", "square", "pentagon", "rectangle"];
@@ -55,3 +58,49 @@ var topics = ["HTML", "CSS", "GIT", "JAVASCRIPT"];
 for (var i = 0; i < topics.length; i++) {
   console.log(topics[i]);
 }
+
+*/
+
+// lets create an array variable again for topics
+
+var topics = ["HTML", "CSS", "GIT", "JAVASCRIPT"];
+
+// creating a random variable using Math objects
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+// console.log(randomTopic);
+
+// creating a function that loops through our topics array...
+
+function listTopics() {
+  for (var i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+  }
+}
+
+// lets now recreate the conditional statement within its own function
+
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Lets study HTML");
+  } else if (randomTopic === "CSS") {
+    console.log("Lets study CSS");
+  } else if (randomTopic === "GIT") {
+    console.log("Lets study GIT");
+  } else if (randomTopic === "JAVASCRIPT") {
+    console.log("Lets study JAVASCRIPT");
+  } else {
+    console.log("Please try again !");
+  }
+}
+
+// invoking the above functions like so:
+
+console.log("HERE IS A LIST OF TOPICS WE LEARNT IN PREWORK");
+
+listTopics();
+
+console.log("Which topic should we study first?");
+
+selectTopic(); // invokes the function and based on the randomTopic variable an output will be displayed in the console...
